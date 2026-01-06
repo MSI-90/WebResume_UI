9b714625ffa449686ad5fe34efbac73c1747a2f0
MSI-90 <s.miller_22@outlook.com>
2026-01-06 13:00:49 +0700

    Исправлена авто-высота блоков конструктора резюме
    Убраны ограничения по высоте, улучшена верстка и стили блока цели резюме

--- web-resume/src/components/Goal.jsx
+++ web-resume/src/components/Goal.jsx
@@ -1,16 +1,27 @@
 import './Goal.css';
+import {useState} from 'react'
 
 export default function Goal() {
+  const [goal, setGoal] = useState(false);
+
   return (
     <>
       <div data-section="purpose-resume" id="item-purpose" className="section item-hidden">
         <div className="item-main-header">
           <h3>Цель резюме</h3>
         </div>
+        {!goal && (
+        <div>
+          <article>
+            Цель резюме — это короткий блок в начале резюме, где ты в 1–3 предложениях объясняешь,
+            какую позицию ищешь и какую пользу принесёшь работодателю.
+          </article>
+        </div>)}
+        {goal && (
         <div className="item-purpose-body">
           <label htmlFor="purpose"></label>
           <textarea id="purpose" spellCheck="true"></textarea>
-        </div>
+        </div>)}
       </div>
     </>
   )
